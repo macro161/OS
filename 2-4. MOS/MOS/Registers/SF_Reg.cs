c3dd08e957c20697706ae4316c890da6a2624648
Kiziela Tomas <tomas.kiziela@mif.stud.vu.lt>
2018-02-14 14:48:15 +0200

    Commit
    Commit

---
+++ 2-4. MOS/MOS/Registers/SF_Reg.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Registers
+{
+    class SF_Reg
+    {
+        private byte _sf; // CF OF XX XXX ZF
+
+        public SF_Reg()
+        {
+            _sf = 0;
+        }
+
+        public void Clear_Reg()
+        {
+            _sf = 0;
+        }
+
+        public void Set_CF()
+        {
+            _sf = (byte)(_sf | 128);
+        }
+
+        public void Set_OF()
+        {
+            _sf = (byte)(_sf | 64);
+        }
+
+        public void Set_ZF()
+        {
+            _sf = (byte)(_sf | 1);
+        }
+
+        public byte Get_SF()
+        {
+            return _sf;
+        }
+        
+
+
+    }
+}
