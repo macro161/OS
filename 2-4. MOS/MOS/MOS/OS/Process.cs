0242df1558c06fbd21303bce0e252f4284f8bac9
Matas Savickis <savickismatas@gmail.com>
2018-05-23 16:58:42 +0300

    Abstract process

--- 2-4. MOS/MOS/MOS/OS/Process.cs
+++ 2-4. MOS/MOS/MOS/OS/Process.cs
@@ -10,11 +10,65 @@ namespace MOS.OS // pagrindine klase is kurios inheritina visi kiti procesai
     abstract class Process
     {
 
-        private static String READY = "READY";
-        private static String RUNNING = "RUNNING";
-        private static String BLOCKED = "BLOCKED";
+        private static string READY = "READY";
+        private static string RUNNING = "RUNNING";
+        private static string BLOCKED = "BLOCKED";
 
-        //public static ArrayList<Process> ready = new ArrayList<Process>;
+        private int priority;
+        private string id;
+        private int pointer;
+        private string status = READY;
+
+        private string[] resource;
+
+        public int GetPointer() {
+            return pointer;
+        }
+
+        public void SetPointer() {
+            this.pointer = pointer;
+        }
+
+        public string GetStatus() {
+            return status;
+        }
+
+        public void SetStatus(string status) {
+            this.status = status;
+        }
+
+        public int GetPriority() {
+            return priority;
+        }
+
+        public void SetPriority(int priority)
+        {
+            this.priority = priority;
+        }
+
+        public string getId() {
+            return id;
+        }
+
+        public void setId(string id) {
+
+        }
+
+        public Process(int priority, string id, string status, int pointer, string[] list) {
+            this.priority = priority;
+            this.id = id;
+            this.status = status;
+            this.pointer = pointer;
+            this.resource = list;
+        }
+
+        public string getResouce() {
+            return this.resource[this.priority];
+        }
+
+        abstract public void run(int pointer);
+        
+        
 
     }
 }
