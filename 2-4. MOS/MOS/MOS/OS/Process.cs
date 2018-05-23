f55d330ea6387682c2f3931d72657d7f6427fa79
Matas Savickis <savickismatas@gmail.com>
2018-05-23 16:37:04 +0300

    Proj failai

--- 2-4. MOS/MOS/MOS/OS/Process.cs
+++ 2-4. MOS/MOS/MOS/OS/Process.cs
@@ -1,12 +1,20 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace MOS.OS
+namespace MOS.OS // pagrindine klase is kurios inheritina visi kiti procesai
 {
-    class Process
+    abstract class Process
     {
+
+        private static String READY = "READY";
+        private static String RUNNING = "RUNNING";
+        private static String BLOCKED = "BLOCKED";
+
+        //public static ArrayList<Process> ready = new ArrayList<Process>;
+
     }
 }
