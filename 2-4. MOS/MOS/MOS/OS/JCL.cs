0b8e147d604187d56d935433a94dbad29d555c6d
just <tvarijonasjustas@gmail.com>
2018-05-23 18:04:41 +0300

    JCL be vieno metodo

--- 2-4. MOS/MOS/MOS/OS/JCL.cs
+++ 2-4. MOS/MOS/MOS/OS/JCL.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MOS.Enums;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,37 @@ using System.Threading.Tasks;
 
 namespace MOS.OS
 {
-    class JCL
+    class JCL : Process
     {
+        public static string[] resources = new String[] { "Check program" };
+        public static JCL jcl = new JCL(65, "JCL", (int)ProcessState.Blocked, 0);
+        public static String output = "";
+        public JCL(int priority, String id, int status, int pointer) : base(priority, id, status, pointer, resources) { }
+
+        public override void Run(int pointer)
+        {
+            if (IsSupervisoryCommandsOk())
+            {
+                Resources.dynamicRes.add("Program ok");
+                Planner.blocked.Add(jcl);
+                // jcl.setPointer(0);
+                jcl.SetStatus((int)ProcessState.Blocked);
+                jcl.SetPriority(jcl.GetPriority() - 1);
+                Planner.LineUp();
+            }
+            else
+            {
+                Resources.dynamicRes.add("Program not ok");
+                Planner.blocked.Add(jcl);
+                // jcl.setPointer(0);
+                jcl.SetStatus((int)ProcessState.Blocked);
+                jcl.SetPriority(jcl.GetPriority() - 1);
+            }
+        }
+
+        private bool IsSupervisoryCommandsOk()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
