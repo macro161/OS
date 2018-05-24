a091ee2b5f3ca614e36defbc007e5aa9e67b7128
Matas Savickis <savickismatas@gmail.com>
2018-05-24 20:33:02 +0300

    Commit

---
+++ 2-4. MOS/MOS/MOS/OS/Planner.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MOS.OS
+{
+    class Planner
+    {
+        private static string READY = "READY";
+        private static string RUNNING = "RUNNING";
+        private static string BLOCKED = "BLOCKED";
+
+        public static List<Process> ready = new List<Process>();
+        public static List<Process> blocked = new List<Process>();
+        public static Process running;
+    }
+}
