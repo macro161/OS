64f247dd41e9e9e8088a1e874750dc6f61cadd85
Matas Savickis <savickismatas@gmail.com>
2018-05-25 19:56:06 +0300

    COmjm

---
+++ 2-4. MOS/MOS/MOS/OS/MemoryResource.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MOS.OS
+{
+    class MemoryResource : Resource
+    {
+        public MemoryResource(Kernel kernel, string name, Process creator, int count, string data) : base(kernel, name, creator, count, data)
+        {
+        }
+
+        List<MemoryResourceElement> resourceElements = new List<MemoryResourceElement>();
+
+        public int ElementAmount { get; set; }
+
+        public int FreeElements { get; set; }
+    }
+
+    class MemoryResourceElement {
+        public int Takelis { get; set; }
+    }
+}
