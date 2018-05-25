874d30dbaa75e93a8434535561f4a1d4a4553470
just <tvarijonasjustas@gmail.com>
2018-05-25 12:44:19 +0300

    pridejau resurso elem klase, nors turbut keisis ten dar

---
+++ 2-4. MOS/MOS/MOS/OS/ResElement.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MOS.OS
+{
+    class ResElement
+    {
+        public Process Receiver { get; set; }
+        public Process Sender { get; set; }
+        public bool ReturnList { get; set; }
+        public ResElement(bool returnList = false,  Process receiver = null, Process sender = null)
+        {
+            Receiver = receiver;
+            Sender = sender;
+            ReturnList = returnList;
+        }
+    }
+}
--- 2-4. MOS/MOS/MOS/OS/Resource.cs
+++ 2-4. MOS/MOS/MOS/OS/Resource.cs
@@ -9,20 +9,24 @@ namespace MOS.OS
     class Resource
     {
         public string Name { get; private set; }
-        public int Elements { get; private set; }
+        public int Count { get; private set; }
         public string Data { get; private set; }
         public Process Creator { get; private set; }
         List<Process> Awaiters { get; set; }
         public Kernel Kernel { get; private set; }
+        public Guid Id { get; private set; }
+        public List<ResElement> Elements { get; set; }
 
-        public Resource(Kernel kernel, string name, Process creator, int elements, string data)
+        public Resource(Kernel kernel, string name, Process creator, int count, string data)
         {
             Kernel = kernel;
             Name = name;
             Creator = creator;
-            Elements = elements;
+            Count = count;
             Data = data;
             Awaiters = new List<Process>();
+            Id = Guid.NewGuid();
+            Elements = new List<ResElement>();
         }
     }
 }
