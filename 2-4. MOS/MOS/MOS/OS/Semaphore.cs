821c283bd12295977c9e3cea42449d7e8e6f8aa7
Matas Savickis <savickismatas@gmail.com>
2018-05-26 14:02:42 +0300

    Sem

---
+++ 2-4. MOS/MOS/MOS/OS/Semaphore.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MOS.OS
+{
+    class Semaphore
+    {
+        private Guid processThatHasAccess;
+        public int trackNumber;
+        private int pointer = 0;
+
+        private List<Guid> processesInQueue = new List<Guid>();
+
+        public Semaphore(int trackNumber)
+        {
+            this.trackNumber = trackNumber;
+        }
+
+        public void Block(Guid id) {
+
+            processesInQueue.Add(id);
+        }
+
+        public bool Write(Guid id) {
+            processThatHasAccess = processesInQueue[pointer];
+
+            if (processThatHasAccess == id)
+            {
+                pointer++;
+                return true;
+            }
+            return false;
+        }
+    }
+}
