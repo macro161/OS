4d0810e3dcbe83ef412b7dcbda702fbbfe35fd24
Matas Savickis <savickismatas@gmail.com>
2018-05-26 14:59:47 +0300

    sadasd

---
+++ 2-4. MOS/MOS/MOS/RealMachine/Semaphore.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MOS.OS
+{
+    class Semaphore
+    {
+        private Guid processThatHasAccess;
+        public int trackNumber;
+
+        public bool Block(Guid id) {
+
+            if (processThatHasAccess == Guid.Empty)
+            {
+                processThatHasAccess = id;
+                return true;
+            }
+            return false;
+        }
+
+        public void Release()
+        {
+            processThatHasAccess = Guid.Empty;
+        }
+    }
+}
